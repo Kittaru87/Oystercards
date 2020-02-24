6cd4accbef3022eaaf5cd95b7428d723c1f4aaf7
Sophia Bell <Kittaru87@gmail.com>
2020-02-24 16:18:57 +0000

    adding journey methods

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,30 +1,50 @@
 
 class Oystercard
 
-  attr_reader :balance, :max_balance
+  attr_reader :balance, :max_balance, :status
 
   MAX_BALANCE = 90
 
   def initialize
     @balance = 0
     @max_balance = MAX_BALANCE
+    @status = false
+  end
+
+  def in_journey?
+    @status
+  end
+
+  def touch_in
+    @status = true
+  end
+
+  def touch_out
+    @status = false
   end
 
   def top_up(money)
     @balance += money
     limit_exceeded(money)
-    "You have topped up by £#{@balance}"
+    balance_message
   end
 
   def deduct(fare)
     @balance -= fare
-    "Your balance is now £#{@balance}"
+    balance_message
   end  
 
+  
+  
+  
   private
 
   def limit_exceeded(money)
     (@balance = @max_balance; raise "Top-up limit exceeded") if @balance > @max_balance
+  end
+
+  def balance_message
+    "Your balance is now £#{@balance}"
   end
 
 
