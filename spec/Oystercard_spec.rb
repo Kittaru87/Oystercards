2a5e0e080f387010d9351e5fb58952f10dd2c305
Sophia Bell <Kittaru87@gmail.com>
2020-02-26 09:12:16 +0000

    correcting require mistake

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,67 +1,67 @@
-require 'oyster'
+require 'Oystercard'
 
-# shared_context "TTT" do
-#   let(:entry_station) { double :entry_station }
-#   let(:exit_station) {double :exit_station }
-#   subject.top_up(50)
-#   subject.touch_in(entry_station)
-#   subject.touch_out(exit_station)
-# end
+  describe OysterCard do
+    let(:entry_station) { double :entry_station }
+    let(:exit_station) {double :exit_station }
 
-describe OysterCard do
-  let(:entry_station) { double :entry_station }
-  let(:exit_station) {double :exit_station }
-# In order to use public transport
-# As a customer
-# I want money on my card
-  it 'shows me my default balance' do
-      expect(subject.balance).to eq OysterCard::DEFAULT_BALANCE
+    it 'shows me my default balance' do
+        expect(subject.balance).to eq OysterCard::DEFAULT_BALANCE
+    end
+
+    it 'no track history as default' do
+      expect(subject).to have_attributes(track_history: [])
+    end 
+
+    describe "#top_up" do
+
+    it 'can top up the balance' do
+      expect{ subject.top_up 1}.to change{ subject.balance }.by 1
+    end
+
+    it "will fail if you try and top up past the maximum balance allowed, #{OysterCard::MAXIMUM_BALANCE}" do
+      expect{subject.top_up(91)}.to raise_error "You cant top up more than #{OysterCard::MAXIMUM_BALANCE} balance"
+    end
+
   end
 
-  it { is_expected.to respond_to(:top_up).with(1).argument }
+  describe "#touch_in" do 
 
-  it 'can top up the balance' do
-    expect{ subject.top_up 1}.to change{ subject.balance }.by 1
-  end
+    it 'will remember entry station on touch in' do
+      subject.top_up(5)
+      expect(subject.touch_in(entry_station)).to eq "You have tapped into #{entry_station}"
+    end
+    
+    it 'returns that I am not in journey if I dont touch in' do
+      expect{subject.in_journey?}.to raise_error 'You are not in journey'
+    end
 
-  it "will fail if you try and top up past the maximum balance allowed, #{OysterCard::MAXIMUM_BALANCE}" do
-    expect{subject.top_up(91)}.to raise_error "You cant top up more than #{OysterCard::MAXIMUM_BALANCE} balance"
-  end
+    it 'will return that the card is in journey if i touch in' do
+      subject.top_up(2)
+      subject.touch_in(entry_station)
+      expect(subject.in_journey?).to eq true
+    end
+
+    it "will not let me through the barrier if my balance is less than #{OysterCard::MINIMUM_FARE}" do
+      expect{subject.touch_in(entry_station)}.to raise_error "You have insufficient funds"
+    end 
   
-  it 'returns that I am not in journey if I dont touch in' do
-    expect{subject.in_journey?}.to raise_error 'You are not in journey'
   end
 
-  it 'will return that the card is in journey if i touch in' do
-    subject.top_up(2)
-    subject.touch_in(entry_station)
-    expect(subject.in_journey?).to eq true
-  end
+  describe "touch_out" do
 
-  it "will not let me through the barrier if my balance is less than #{OysterCard::MINIMUM_FARE}" do
-    expect{subject.touch_in(entry_station)}.to raise_error "You have insufficient funds"
-  end 
-  
-  it "deduct correct fare from balance" do
-    subject.top_up(40)
-    subject.touch_in(entry_station)
-    expect {subject.touch_out(exit_station)}.to change{subject.balance}.by(-OysterCard::MINIMUM_FARE)
-  end
+    it "deduct correct fare from balance" do
+      subject.top_up(40)
+      subject.touch_in(entry_station)
+      expect {subject.touch_out(exit_station)}.to change{subject.balance}.by(-OysterCard::MINIMUM_FARE)
+    end
 
-  it 'will remember entry station on touch in' do
-    subject.top_up(5)
-    expect(subject.touch_in(entry_station)).to eq "You have tapped into #{entry_station}"
-  end
+    it 'it will remember exit station on tap out' do
+      subject.top_up(50)
+      subject.touch_in(entry_station)
+      expect(subject.touch_out(exit_station)).to eq "You have tapped out of #{exit_station}"
+    end
 
-  it 'it will remember exit station on tap out' do
-    subject.top_up(50)
-    subject.touch_in(entry_station)
-    expect(subject.touch_out(exit_station)).to eq "You have tapped out of #{exit_station}"
   end
-
-  it 'no track history as default' do
-    expect(subject).to have_attributes(track_history: [])
-  end 
   
   context 'TTT' do
     before(:each) do
@@ -69,6 +69,7 @@ describe OysterCard do
       subject.touch_in(entry_station)
       subject.touch_out(exit_station)
     end
+
     it 'will forget entry station on touch out' do
       expect(subject).to have_attributes(entry_station: nil)
     end
