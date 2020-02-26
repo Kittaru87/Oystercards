e776d04608b3ccc68b97321eed724e1963d95422
Sophia Bell <Kittaru87@gmail.com>
2020-02-26 09:02:40 +0000

    tests for touch in/out/refactoring

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,58 +1,85 @@
-require 'Oystercard'
+require 'oyster'
 
-describe Oystercard do
+# shared_context "TTT" do
+#   let(:entry_station) { double :entry_station }
+#   let(:exit_station) {double :exit_station }
+#   subject.top_up(50)
+#   subject.touch_in(entry_station)
+#   subject.touch_out(exit_station)
+# end
 
-  it "should have a default balance of 0" do
-    expect(subject).to have_attributes(balance: 0)
+describe OysterCard do
+  let(:entry_station) { double :entry_station }
+  let(:exit_station) {double :exit_station }
+# In order to use public transport
+# As a customer
+# I want money on my card
+  it 'shows me my default balance' do
+      expect(subject.balance).to eq OysterCard::DEFAULT_BALANCE
   end
-  
+
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  describe "#top_up" do
+  it 'can top up the balance' do
+    expect{ subject.top_up 1}.to change{ subject.balance }.by 1
+  end
+
+  it "will fail if you try and top up past the maximum balance allowed, #{OysterCard::MAXIMUM_BALANCE}" do
+    expect{subject.top_up(91)}.to raise_error "You cant top up more than #{OysterCard::MAXIMUM_BALANCE} balance"
+  end
   
-    it "should top up the balance by 20" do
-      expect { subject.top_up(20) }.to change { subject.balance }.by(20)
-    end
+  it 'returns that I am not in journey if I dont touch in' do
+    expect{subject.in_journey?}.to raise_error 'You are not in journey'
+  end
 
-    it "should tell the customer how much they've topped up by" do
-      expect(subject.top_up(20)).to eq "Your balance is now £20"
-    end
+  it 'will return that the card is in journey if i touch in' do
+    subject.top_up(2)
+    subject.touch_in(entry_station)
+    expect(subject.in_journey?).to eq true
+  end
 
-    it "should raise an error if balance limit is exceeded" do
-      expect { subject.top_up(95) }.to raise_error("Top-up limit exceeded")
-    end
+  it "will not let me through the barrier if my balance is less than #{OysterCard::MINIMUM_FARE}" do
+    expect{subject.touch_in(entry_station)}.to raise_error "You have insufficient funds"
+  end 
+  
+  it "deduct correct fare from balance" do
+    subject.top_up(40)
+    subject.touch_in(entry_station)
+    expect {subject.touch_out(exit_station)}.to change{subject.balance}.by(-OysterCard::MINIMUM_FARE)
+  end
 
-    it "should raise an error if balance limit is exceeded" do
+  it 'will remember entry station on touch in' do
+    subject.top_up(5)
+    expect(subject.touch_in(entry_station)).to eq "You have tapped into #{entry_station}"
+  end
+
+  it 'it will remember exit station on tap out' do
+    subject.top_up(50)
+    subject.touch_in(entry_station)
+    expect(subject.touch_out(exit_station)).to eq "You have tapped out of #{exit_station}"
+  end
+
+  it 'no track history as default' do
+    expect(subject).to have_attributes(track_history: [])
+  end 
+  
+  context 'TTT' do
+    before(:each) do
       subject.top_up(50)
-      expect { subject.top_up(45) }.to raise_error("Top-up limit exceeded")
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+    end
+    it 'will forget entry station on touch out' do
+      expect(subject).to have_attributes(entry_station: nil)
     end
 
+    it 'returns the exit station when required' do
+      expect(subject).to have_attributes(exit_station: exit_station)
+    end
+
+    it 'expect track history to store entry and exit station upon touch out' do
+      expect(subject).to have_attributes(track_history: [{ entry_station=>exit_station }])
+    end
   end
-
-  describe "#deduct" do
-
-    it "should deduct fare from the balance" do
-      subject.top_up(20)  
-      expect { subject.deduct(5) }.to change { subject.balance }.by(-5)
-    end
-
-  end
-
-  describe "#in_journey" do
-
-    it { is_expected.to respond_to :in_journey? }
-
-    it "when touching in, the oystercard state is true" do
-      subject.touch_in
-      expect(subject).to have_attributes(status: true)
-    end
-    it "when touching out, the oystercard state is false" do
-      subject.touch_out
-      expect(subject).to have_attributes(status: false) 
-    end
-
-  end
-
-
     
 end
