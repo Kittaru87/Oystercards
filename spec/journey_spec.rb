4b6e07789fb8d340e493cca279539b22b7c2f46b
Sophia Bell <Kittaru87@gmail.com>
2020-02-27 11:55:24 +0000

    changing tests for new hash in journey class

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,15 +2,11 @@ require 'journey'
 
 describe Journey do
 
-    let(:station) { double :station, zone: 1}
-    let(:other_station) { double :other_station, zone: 2 }
+    let(:station) { double(name: :station, zone: 1)}
+    let(:other_station) { double(name: :other_station, zone: 1)}
 
-    it 'responds to entry station' do
-        expect(subject.start_journey(station)).to eq station
-    end
-
-    it 'responds to exit station' do
-        expect(subject.end_journey(other_station)).to eq other_station
+    it 'initializes with an empty journey' do
+        expect(subject).to have_attributes(current_journey = {})
     end
 
     it 'calculates a fare' do
@@ -32,5 +28,16 @@ describe Journey do
     it "returns a penalty fare if no exit station given" do
         expect(subject.fare).to eq Journey::PENALTY
     end
-    
+
+    it 'expect current_journey to store entry and exit station upon touch out' do
+        subject.start_journey(station)
+        subject.end_journey(other_station)
+        expect(subject).to have_attributes(current_journey: { :departures=>station.name,:arrivals=>other_station.name })
+      end
+
+    it 'will return that the card is in journey if i touch in' do
+      subject.start_journey(station)
+      expect(subject.in_journey?).to eq true
+    end
+
 end
