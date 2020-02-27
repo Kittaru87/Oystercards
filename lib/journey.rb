fb47d44094e0c2ffacc1c3db2380b0243dc7267d
Sophia Bell <Kittaru87@gmail.com>
2020-02-27 11:53:39 +0000

    creating a proper hash for the current journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,7 +2,7 @@ require_relative 'station'
 
 class Journey
 
-    attr_reader :entry_station, :exit_station
+    attr_reader :entry_station, :exit_station, :current_journey
 
     MIN_FARE = 1
     PENALTY = 6
@@ -10,18 +10,25 @@ class Journey
     def initialize
         @entry_station
         @exit_station
+        @current_journey = {departures: nil, arrivals: nil}
     end
 
     def start_journey(station)
-        @entry_station = station
+        @entry_station = station.name
+        @current_journey[:departures] = @entry_station
     end
 
     def end_journey(station)
-        @exit_station = station
+        @exit_station = station.name
+        @current_journey[:arrivals] = @exit_station
     end
 
     def complete?
         @entry_station && @exit_station
+    end
+
+    def in_journey?
+        @current_journey.keys != [] ? true : false
     end
 
     def fare
